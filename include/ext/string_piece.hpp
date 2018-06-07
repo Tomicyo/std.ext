a7a9f05ac79dc62e32de3bbb9f278d661cb17ccd
dsotsin <alexqzhou@tencent.com>
2018-06-07 11:18:54 +0800

    update string piece

--- include/ext/string_piece.hpp
+++ include/ext/string_piece.hpp
@@ -6,40 +6,43 @@
 
 namespace std
 {
-	template <typename T>
-	class base_string_piece
+	namespace ext
 	{
-	public:
-		typedef size_t size_type;
-		typedef typename T::value_type value_type;
-		typedef const value_type* const_iterator;
-
-		constexpr base_string_piece() : m_ptr(nullptr), m_length(0) {}
-		constexpr base_string_piece(const value_type* str) : m_ptr(str), m_length(char_traits<value_type>::length(str)) {}
-		constexpr base_string_piece(const value_type* offset, size_type len) : m_ptr(offset), m_length(len) {}
-		base_string_piece(const typename T::const_iterator& begin, const typename T::const_iterator& end)
+		template <typename T>
+		class base_string_piece
 		{
-			m_length = distance(begin, end);
-			m_ptr = m_length > 0 ? &*begin : nullptr;
-		}
+		public:
+			typedef size_t size_type;
+			typedef typename T::value_type value_type;
+			typedef const value_type* const_iterator;
 
-		bool is_null() const { return m_ptr == nullptr; }
-		constexpr const value_type* data() const { return m_ptr; }
-		constexpr size_type size() const { return m_length; }
-		constexpr size_type length() const { return m_length; }
+			constexpr base_string_piece() : m_ptr(nullptr), m_length(0) {}
+			constexpr base_string_piece(const value_type* str) : m_ptr(str), m_length(char_traits<value_type>::length(str)) {}
+			constexpr base_string_piece(const value_type* offset, size_type len) : m_ptr(offset), m_length(len) {}
+			base_string_piece(const typename T::const_iterator& begin, const typename T::const_iterator& end)
+			{
+				m_length = distance(begin, end);
+				m_ptr = m_length > 0 ? &*begin : nullptr;
+			}
 
-		const_iterator begin() const { return m_ptr; }
-		const_iterator end() const { return m_ptr + m_length; }
+			bool is_null() const { return m_ptr == nullptr; }
+			constexpr const value_type* data() const { return m_ptr; }
+			constexpr size_type size() const { return m_length; }
+			constexpr size_type length() const { return m_length; }
 
-		T to_string() const { return is_null() ? T() : T(data(), size()); }
-		explicit operator T() const { return to_string(); }
+			const_iterator begin() const { return m_ptr; }
+			const_iterator end() const { return m_ptr + m_length; }
 
-	private:
-		const value_type* m_ptr;
-		size_type m_length;
-	};
+			T to_string() const { return is_null() ? T() : T(data(), size()); }
+			explicit operator T() const { return to_string(); }
 
-	typedef base_string_piece<string> string_piece;
+		private:
+			const value_type* m_ptr;
+			size_type m_length;
+		};
+
+		typedef base_string_piece<string> string_piece;
+	}
 }
 
 #endif
